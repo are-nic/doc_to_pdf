84b6f30aecd057829660d1d56e89dbbfea137461
repair-cat <simf61@yandex.ru>
2020-03-27 09:30:17 +0300

    changed paths of directory files

--- init_vbscript.py
+++ init_vbscript.py
@@ -1,10 +1,16 @@
 import os
 
-folder_from = os.getcwd() + r'\words'   # папка, где лежит тонна word файлов
-folder_to = os.getcwd() + r'\txts'      # папка, куда будем сохранять 
+folder_from = os.getcwd() + r'\Desktop\Python books\docs'   # папка, где лежит тонна word файлов
+folder_to = os.getcwd() + r'\Desktop\ежегодная проверка знаний'   # папка, куда будем сохранять 
 
 
 def convert(file_name):
-    str1 = folder_from + r"/" + file_name
-    str2 = folder_to + r"/" + file_name[:file_name.rfind('.')] 
-    os.system('converter.vbs  "' + str1 + '" "' + str2 + '"')   # запуск скрипта
+    path_to_vbs = r'C:\Users\Руслан\Documents\doc_to_pdf\\'        
+    path_to_docs = folder_from + r"/" + file_name
+    path_for_pdf = folder_to + r"/" + file_name[:file_name.rfind('.')] 
+    os.system('CScript {}converter.vbs "{}" "{}"'.format(path_to_vbs, path_to_docs, path_for_pdf))   # запуск скрипта в cmd
+
+
+for subdir, dirs, files in os.walk(folder_from):
+    for file in files:
+        convert(file)
