a202191aec39d3875e8b97a14ba8f4f81db6391a
repair-cat <simf61@yandex.ru>
2020-04-21 22:32:01 +0300

    added class Convertation

--- init_vbscript.py
+++ init_vbscript.py
@@ -1,16 +1,20 @@
 import os
 
-folder_from = os.getcwd() + r'\Desktop\Python books\docs'   # папка, где лежит тонна word файлов
-folder_to = os.getcwd() + r'\Desktop\ежегодная проверка знаний'   # папка, куда будем сохранять 
+#folder_from = os.getcwd() + r'\Desktop\Python books\docs'   # папка, где лежит тонна word файлов
+#folder_to = os.getcwd() + r'\Desktop\ежегодная проверка знаний'   # папка, куда будем сохранять 
 
 
-def convert(file_name):
-    path_to_vbs = r'C:\Users\Руслан\Documents\doc_to_pdf\\'        
-    path_to_docs = folder_from + r"/" + file_name
-    path_for_pdf = folder_to + r"/" + file_name[:file_name.rfind('.')] 
-    os.system('CScript {}converter.vbs "{}" "{}"'.format(path_to_vbs, path_to_docs, path_for_pdf))   # запуск скрипта в cmd
+class Convertation:
+    def __init__(self, folder_from, folder_to):
+        self.folder_from = folder_from
+        self.folder_to = folder_to
 
 
-for subdir, dirs, files in os.walk(folder_from):
-    for file in files:
-        convert(file)
+    def convert(self):
+        path_to_vbs = os.getcwd() + r'\converter.vbs'
+        for subdir, dirs, files in os.walk(self.folder_from):
+            for file_name in files:
+                path_to_docs = self.folder_from + r"/" + file_name
+                path_for_pdf = self.folder_to + r"/" + file_name[:file_name.rfind('.')] 
+                # запуск скрипта в cmd
+                os.system('CScript {}converter.vbs "{}" "{}"'.format(path_to_vbs, path_to_docs, path_for_pdf))
